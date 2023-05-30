ec9cf1d83685b5f36d386ad5a24c4cf9f561a668
Thad Osburn <thadmsosburn@outlook.com>
2023-05-30 08:56:27 -0400

    Update

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
@@ -2,4 +2,69 @@ package com.techelevator;
 
 public class Airplane {
 
+            //Variables
+    private String planeNumber;
+    private int totalFirstClassSeats;
+    private int bookedFirstClassSeats;
+    private int totalCoachSeats;
+    private int bookedCoachSeats;
+
+
+
+            //Getters
+
+    public String getPlaneNumber() {
+        return planeNumber;
+    }
+
+    public int getTotalFirstClassSeats() {
+        return totalFirstClassSeats;
+    }
+
+    public int getBookedFirstClassSeats() {
+        return bookedFirstClassSeats;
+    }
+
+    public int getTotalCoachSeats() {
+        return totalCoachSeats;
+    }
+
+    public int getBookedCoachSeats() {
+        return bookedCoachSeats;
+    }
+
+
+    // Derived
+    public int getAvailableFirstClassSeats() {
+       return totalFirstClassSeats - bookedFirstClassSeats;
+    }
+    public int getAvailableCoachSeats(){
+        return totalCoachSeats - bookedCoachSeats;
+    }
+
+
+
+
+            //Constructors
+    public Airplane (String planeNumber, int totalFirstClassSeats, int totalCoachSeats){
+        this.planeNumber = planeNumber;
+        this.totalFirstClassSeats = totalFirstClassSeats;
+        this.totalCoachSeats = totalCoachSeats;
+    }
+
+
+
+            //Methods
+    public boolean reserveSeats (boolean forFirstClass, int totalNumberOfSeats){
+        if (forFirstClass == true && this.getAvailableFirstClassSeats() >= totalNumberOfSeats){
+            bookedFirstClassSeats += totalNumberOfSeats;
+            return true;
+        } else if (forFirstClass == false && this.getAvailableCoachSeats() >= totalNumberOfSeats) {
+            bookedCoachSeats += totalNumberOfSeats;
+            return true;
+        }
+        return false;
+
+    }
+
 }
