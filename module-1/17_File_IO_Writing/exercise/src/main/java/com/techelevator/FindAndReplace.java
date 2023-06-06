cbc75b4b7be58ba869bd8c4457f19bd96f8f07f7
Thad Osburn <thadmsosburn@outlook.com>
2023-06-06 18:18:20 -0400

    File IO

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,5 +1,9 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class FindAndReplace {
@@ -14,8 +18,33 @@ public class FindAndReplace {
 
     public void run() {
         /* Your code goes here */
+        System.out.println("What word would you like to replace?");
 
+        String wordToReplace = userInput.nextLine();
 
+        System.out.println("What word would you like in its place?");
+
+        String newWord = userInput.nextLine();
+
+        System.out.println("What file would you like to use?");
+
+        String txtFile = userInput.nextLine();
+
+        System.out.println("What file would you like to import to?");
+
+        String outputFile = userInput.nextLine();
+        if (Objects.equals(wordToReplace, "")){
+            System.out.println(txtFile);
+        }
+        try (Scanner txtInput = new Scanner(new File(txtFile));
+             PrintWriter txtOutput = new PrintWriter(outputFile)){
+            while (txtInput.hasNextLine()) {
+                String lineOfInput = txtInput.nextLine();
+                txtOutput.println(lineOfInput.replace(wordToReplace,newWord));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,21 +1,49 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FizzWriter {
 
-	// Use this scanner for all user input. Don't create additional Scanners with System.in
-	private final Scanner userInput = new Scanner(System.in);
+    // Use this scanner for all user input. Don't create additional Scanners with System.in
+    private final Scanner userInput = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		FizzWriter fizzWriter = new FizzWriter();
-		fizzWriter.run();
-	}
+    public static void main(String[] args) throws FileNotFoundException {
+        FizzWriter fizzWriter = new FizzWriter();
+        fizzWriter.run();
+    }
 
-	public void run() {
-		/* Your code goes here */
+    public void run() throws FileNotFoundException {
+        /* Your code goes here */
+        System.out.print("Output file: ");
+
+        String name = userInput.nextLine();
+        try (PrintWriter fizzBuzzWriter = new PrintWriter(name)) {
 
 
-	}
+            for (int i = 1; i <= 300; i++) {
+
+
+                if (i % 3 == 0 && i % 5 == 0) {
+                    fizzBuzzWriter.println("FizzBuzz");
+                } else if (i % 5 == 0) {
+                    fizzBuzzWriter.println("Buzz");
+                } else if (i % 3 == 0) {
+                    fizzBuzzWriter.println("Fizz");
+                } else {
+                    fizzBuzzWriter.println(i);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
 
 }
