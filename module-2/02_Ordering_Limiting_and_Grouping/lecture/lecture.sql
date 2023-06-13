502e95690cb06c3700e1878fd351b0439054b808
Thad Osburn <thadmsosburn@outlook.com>
2023-06-13 12:15:02 -0400

    SQL Homework

--- module-2/02_Ordering_Limiting_and_Grouping/lecture/lecture.sql
+++ module-2/02_Ordering_Limiting_and_Grouping/lecture/lecture.sql
@@ -1,50 +1,73 @@
 -- ORDERING RESULTS
 
 -- Populations of all states from largest to smallest.
+SELECT population, state_name FROM state
+ORDER BY population DESC;
 
-
--- States sorted alphabetically (A-Z) within their census region. The census regions are sorted in reverse alphabetical (Z-A) order.
-
+-- States sorted alphabetically (A-Z) within their census region. 
+--The census regions are sorted in reverse alphabetical (Z-A) order.
+SELECT state_name, population, area, census_region FROM state
+ORDER BY census_region DESC, state_name ASC;
 
 -- The biggest park by area
-
+SELECT * FROM park
+ORDER BY area DESC;
 
 
 -- LIMITING RESULTS
 
 -- The 10 largest cities by populations
-
+SELECT * FROM city
+ORDER BY population DESC
+LIMIT 10;
 
 -- The 20 oldest parks from oldest to youngest in years, sorted alphabetically by name.
-
-
-
+SELECT ((CURRENT_DATE - date_established) / 365) AS age, park_name
+FROM park
+ORDER BY age DESC, park_name ASC
+LIMIT 20;
 
 -- CONCATENATING OUTPUTS
 
 -- All city names and their state abbreviation.
-
+SELECT (city_name || ' (' || state_abbreviation || ')') AS city_and_state 
+FROM city
+ORDER BY city_name;
 
 -- The all parks by name and date established.
-
-
+SELECT ('Name: ' || park_name || ' Date Established: ' || date_established) AS park_and_established_date
+FROM park
+ORDER BY park_name;
 -- The census region and state name of all states in the West & Midwest sorted in ascending order.
+SELECT (census_region || ': ' || state_name) AS region_and_state
+FROM state
+WHERE census_region IN ('West', 'Midwest')
+ORDER BY region_and_state;
 
 
+-- Numeric Functions
+SELECT *, round(area / population, 6)
+FROM city;
 
 -- AGGREGATE FUNCTIONS
 
 -- Average population across all the states. Note the use of alias, common with aggregated values.
-
+SELECT AVG(population) AS avg_state_population
+FROM state;
 
 -- Total population in the West and South census regions
-
+SELECT SUM(population) AS west_south_population
+FROM state
+WHERE census_region IN ('West','South');
 
 -- The number of cities with populations greater than 1 million
-
+SELECT COUNT(population)
+FROM city
+WHERE population > 1000000;
 
 -- The number of state nicknames.
-
+SELECT COUNT(state_nickname) AS nickname_count, COUNT(*) AS row_count
+FROM state;
 
 -- The area of the smallest and largest parks.
 
