ecc8f73e9f218fa9a326c1643720d100afdfc389
Thad Osburn <thadmsosburn@outlook.com>
2023-06-21 10:11:59 -0400

    Morning Push

--- module-2/07_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcCollectionDao.java
+++ module-2/07_Data_Access_Part_1/exercise/src/main/java/com/techelevator/movies/dao/JdbcCollectionDao.java
@@ -2,6 +2,7 @@ package com.techelevator.movies.dao;
 
 import com.techelevator.movies.model.Collection;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
@@ -17,11 +18,27 @@ public class JdbcCollectionDao implements CollectionDao{
 
     @Override
     public List<Collection> getCollections() {
-        return null;
+        List<Collection> collections = new ArrayList<>();
+        String sql = "SELECT collection_id, collection_name FROM collection";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while (results.next()) {
+            Collection collection = new Collection();
+            collection.setId(results.getInt("collection_id"));
+            collection.setName(results.getString("collection_name"));
+            collections.add(collection);
+        }
+        return collections;
     }
 
     @Override
     public Collection getCollectionById(int id) {
+        Collection collection = null;
+        String sql = "SELECT collection_id FROM collection WHERE collection_name = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql,id);
+        while (results.next()){
+        collection = results.getObject("collection_id",id);
+
+        }
         return new Collection(-1, "Not implemented yet");
     }
 
