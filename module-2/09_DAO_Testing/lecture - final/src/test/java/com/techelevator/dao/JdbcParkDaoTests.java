cb083175bd4d850e69689a7264a55cc42d3ca0a2
Brian Lauvray <brian@techelevator.com>
2023-06-22 12:44:28 -0400

    lecture code

--- module-2/09_DAO_Testing/lecture - final/src/test/java/com/techelevator/dao/JdbcParkDaoTests.java
+++ module-2/09_DAO_Testing/lecture - final/src/test/java/com/techelevator/dao/JdbcParkDaoTests.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class JdbcParkDaoTests extends BaseDaoTests {
 
@@ -16,56 +17,100 @@ public class JdbcParkDaoTests extends BaseDaoTests {
     private static final Park PARK_3 =
             new Park(3, "Park 3", LocalDate.parse("2000-06-15"), 300, false);
 
+    private Park testPark;
     private JdbcParkDao sut;
 
     @Before
     public void setup() {
         sut = new JdbcParkDao(dataSource);
+        testPark = new Park(0, "TEST PARK", LocalDate.now(), 0, false);
     }
 
     @Test
     public void getParkById_returns_correct_park() {
-        Assert.fail();
+        Park parkReturned = sut.getParkById(1);
+        Assert.assertEquals(PARK_1, parkReturned);
+
+        parkReturned = sut.getParkById(3);
+        Assert.assertEquals(PARK_3, parkReturned);
     }
 
     @Test
     public void getParkById_returns_null_when_id_not_found() {
-        Assert.fail();
+        Park parkReturned = sut.getParkById(99);
+        Assert.assertNull( parkReturned );
     }
 
     @Test
     public void getParksByState_returns_all_parks_for_state() {
-        Assert.fail();
+        List<Park> parksReturned = sut.getParksByState("AA");
+        Assert.assertEquals("Wrong number of parks", 2, parksReturned.size());
+        Assert.assertTrue( parksReturned.contains( PARK_1 ) );
+        Assert.assertTrue( parksReturned.contains( PARK_3 ) );
+
+        parksReturned = sut.getParksByState("BB");
+        Assert.assertEquals(1, parksReturned.size());
+        Assert.assertTrue( parksReturned.contains(PARK_2) );
     }
 
     @Test
     public void getParksByState_returns_empty_list_for_abbreviation_not_in_db() {
-        Assert.fail();
+        List<Park> parksReturned = sut.getParksByState("XX");
+        Assert.assertEquals(0, parksReturned.size());
     }
 
     @Test
     public void createPark_returns_park_with_id_and_expected_values() {
-        Assert.fail();
+        Park createdPark = sut.createPark(testPark);
+
+        int newParkId = createdPark.getParkId();
+        Assert.assertTrue( newParkId > 0 );
+
+        Park retrievedPark = sut.getParkById( newParkId );
+
+        Assert.assertEquals(createdPark, retrievedPark);
     }
 
     @Test
     public void updated_park_has_expected_values_when_retrieved() {
-        Assert.fail();
+        Park parkToUpdate = sut.getParkById(1);
+
+        parkToUpdate.setParkName("UPDATED");
+        parkToUpdate.setDateEstablished(LocalDate.now());
+        parkToUpdate.setArea(999);
+        parkToUpdate.setHasCamping(false);
+
+        sut.updatePark( parkToUpdate );
+
+        Park updatedPark = sut.getParkById(1);
+
+        Assert.assertEquals(parkToUpdate, updatedPark);
     }
 
     @Test
     public void deleted_park_cant_be_retrieved() {
-        Assert.fail();
+        sut.deleteParkById(1);
+        Assert.assertNull( sut.getParkById(1 ));
     }
 
     @Test
     public void park_added_to_state_is_in_list_of_parks_by_state() {
-        Assert.fail();
+        sut.linkParkState(2, "CC");
+        List<Park> parksReturned = sut.getParksByState("CC");
+        Assert.assertEquals(2, parksReturned.size());
+        Assert.assertTrue( parksReturned.contains( PARK_2) );
     }
 
     @Test
     public void park_removed_from_state_is_not_in_list_of_parks_by_state() {
-        Assert.fail();
+        sut.unlinkParkState(1, "AA");
+        List<Park> parksReturned = sut.getParksByState("AA");
+        Assert.assertEquals(1, parksReturned.size());
+        Assert.assertFalse( parksReturned.contains(PARK_1) );
+
+        sut.unlinkParkState(3, "AA");
+        parksReturned = sut.getParksByState("AA");
+        Assert.assertEquals(0, parksReturned.size());
     }
 
     private void assertParksMatch(Park expected, Park actual) {
