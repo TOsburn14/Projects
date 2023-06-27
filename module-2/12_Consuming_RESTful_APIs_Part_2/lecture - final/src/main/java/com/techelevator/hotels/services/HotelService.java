e4836d373c0ec14235d1d7c7f826b25ce813c7d8
Brian Lauvray <brian@techelevator.com>
2023-06-27 12:44:54 -0400

    Lecture code

--- module-2/12_Consuming_RESTful_APIs_Part_2/lecture - final/src/main/java/com/techelevator/hotels/services/HotelService.java
+++ module-2/12_Consuming_RESTful_APIs_Part_2/lecture - final/src/main/java/com/techelevator/hotels/services/HotelService.java
@@ -3,6 +3,9 @@ package com.techelevator.hotels.services;
 import com.techelevator.hotels.model.Hotel;
 import com.techelevator.hotels.model.Reservation;
 import com.techelevator.util.BasicLogger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
@@ -18,6 +21,19 @@ public class HotelService {
      */
     public Hotel[] listHotels() {
         Hotel[] hotels = null;
+
+        try {
+
+            hotels = restTemplate.getForObject(API_BASE_URL + "hotels", Hotel[].class);
+        } catch (ResourceAccessException e) {
+            // ResourceAccessException is thrown when the server cannot be reached
+            // for example: no internet connection, server down, wrong address, etc.
+            BasicLogger.log("Unable to connect to server");
+        } catch (RestClientResponseException e) {
+            // RestClientResponseException is thrown when the server responds with
+            // a 4xx or 5xx status code
+            BasicLogger.log(e.getRawStatusCode() + " : " + e.getStatusText());
+        }
         return hotels;
     }
 
@@ -25,8 +41,26 @@ public class HotelService {
      * Create a new reservation in the hotel reservation system
      */
     public Reservation addReservation(Reservation newReservation) {
-        // TODO: Implement method
-        return null;
+
+        String url = API_BASE_URL + "hotels/" +  newReservation.getHotelId() + "/reservations";
+
+        // 1) Create a Header and set the content type to JSON
+        // 2) Set the message body to the reservation object
+        // 3) Send the request with the header and the message body (from 1 and 2)
+        // 4) Receive the response and return it
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType( MediaType.APPLICATION_JSON );
+        HttpEntity<Reservation> httpEntity = new HttpEntity<Reservation>(newReservation, headers);
+
+        Reservation returnedReservation = null;
+        try {
+            returnedReservation = restTemplate.postForObject(url, httpEntity, Reservation.class);
+        } catch (ResourceAccessException e) {
+            BasicLogger.log("Unable to connect to server");
+        } catch (RestClientResponseException e) {
+            BasicLogger.log(e.getRawStatusCode() + " : " + e.getStatusText());
+        }
+        return returnedReservation;
     }
 
     /**
@@ -34,16 +68,47 @@ public class HotelService {
      * reservation
      */
     public boolean updateReservation(Reservation updatedReservation) {
-        // TODO: Implement method
-        return false;
+        boolean wasSuccessful = false;
+        String url = API_BASE_URL + "reservations/" + updatedReservation.getId();
+
+        // 1) Create header object
+        // 2) Set the Content Type to application/json
+        // 3) Create an HttpEntity<T> where T is the data type that will form the message body
+        // 4) Pass the HttpEntity the object (reservation) with the data and the headers
+        // 5) Send the put request using the put() method of the RestTemplate
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType( MediaType.APPLICATION_JSON );
+        HttpEntity<Reservation> httpEntity = new HttpEntity<Reservation>(updatedReservation, headers);
+
+        try {
+            restTemplate.put(url, httpEntity);
+            wasSuccessful = true;
+        } catch (ResourceAccessException e) {
+            BasicLogger.log("Unable to connect to Server");
+        } catch (RestClientResponseException e) {
+            BasicLogger.log(e.getRawStatusCode() + " : " + e.getStatusText());
+        }
+
+        return wasSuccessful;
     }
 
     /**
      * Delete an existing reservation
      */
     public boolean deleteReservation(int id) {
-        // TODO: Implement method
-        return false;
+
+        boolean wasSuccessful = false;
+        try {
+            // 1. Call the delete() method on the RestTemplate and pass it the URL
+            restTemplate.delete(API_BASE_URL + "reservations/" + id );
+            wasSuccessful = true;
+        } catch (ResourceAccessException e) {
+            BasicLogger.log("Cannot connect to server");
+        } catch (RestClientResponseException e) {
+            BasicLogger.log(e.getRawStatusCode() + " : " + e.getStatusText());
+        }
+
+        return wasSuccessful;
     }
 
     /* DON'T MODIFY ANY METHODS BELOW */
