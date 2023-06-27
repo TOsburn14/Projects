9951dbf8dbad7995719bd3eed3a81def48d60660
Brian Lauvray <brian@techelevator.com>
2023-06-27 16:43:51 -0400

    Added Server Side APIs pt 1 lecture and pt 2 tutorial

---
+++ module-2/13_Server_Side_APIs_Part_1/lecture - final/server/src/main/java/com/techelevator/reservations/controllers/HotelController.java
@@ -0,0 +1,40 @@
+package com.techelevator.reservations.controllers;
+
+import com.techelevator.reservations.dao.HotelDao;
+import com.techelevator.reservations.dao.MemoryHotelDao;
+import com.techelevator.reservations.dao.MemoryReservationDao;
+import com.techelevator.reservations.dao.ReservationDao;
+import com.techelevator.reservations.model.Hotel;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class HotelController {
+
+    private HotelDao hotelDao;
+    private ReservationDao reservationDao;
+
+    public HotelController() {
+        this.hotelDao = new MemoryHotelDao();
+        this.reservationDao = new MemoryReservationDao(hotelDao);
+    }
+
+    /**
+     * Return All Hotels
+     *
+     * @return a list of all hotels in the system
+     */
+
+
+    /**
+     * Return hotel information
+     *
+     * @param id the id of the hotel
+     * @return all info for a given hotel
+     */
+
+
+}
