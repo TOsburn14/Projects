4874a5d6855f66327d098fa43e838abcfe6630e5
Brian Lauvray <brian@techelevator.com>
2023-07-17 18:19:41 -0400

    Added JS Events lecture

---
+++ module-3/09_Event_Handling/lecture/1-event-handling/annoyboard.js
@@ -0,0 +1,19 @@
+const audio = new Audio('assets/sounds/duck.wav');
+
+
+    const toggle = document.getElementById('switch').querySelector("input[type=checkbox]");
+
+
+
+function startAnnoying() {
+
+}
+
+function stopAnnoying() {
+
+}
+
+function doAnnoy() {
+    audio.play();
+    console.log("key")
+}
